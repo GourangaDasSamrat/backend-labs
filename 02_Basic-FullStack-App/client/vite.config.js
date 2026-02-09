8618edd02a268e932c76dbd822229bedfc245e1c
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-09 16:49:47 +0600

    feat(client): Add API proxy for client
    - Configured Vite dev server proxy
    - Forward /api requests to backend URL
    - Updated .env.example variable name

--- 02_Basic-FullStack-App/client/vite.config.js
+++ 02_Basic-FullStack-App/client/vite.config.js
@@ -1,15 +1,27 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_JOKE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
-    }),
-  ],
+    },
+    plugins: [
+      tailwindcss(),
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+    ],
+  };
 });

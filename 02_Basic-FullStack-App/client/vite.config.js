f0d28c0159619f4fa487a8ad8284baa50ae2bd83
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-09 17:40:11 +0600

    chore(env): remove env system

--- 02_Basic-FullStack-App/client/vite.config.js
+++ 02_Basic-FullStack-App/client/vite.config.js
@@ -1,27 +1,23 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_JOKE_API_BASE_URL,
-          changeOrigin: true,
-          secure: false,
-        },
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
       },
     },
-    plugins: [
-      tailwindcss(),
-      react({
-        babel: {
-          plugins: [["babel-plugin-react-compiler"]],
-        },
-      }),
-    ],
-  };
+  },
+  plugins: [
+    tailwindcss(),
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
 });

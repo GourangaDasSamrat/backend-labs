6fedd61a9468e83f9ca1e4279848887441e9b130
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-11 16:07:21 +0600

    feat(todos): add fields to Todo model
    - Make `content` field required
    - Add `complete` status
    - Link `createdBy` to User model
    - Include `subTodos` array

--- 01_Basic-Bakcend/models/todos/todo.models.js
+++ 01_Basic-Bakcend/models/todos/todo.models.js
@@ -4,7 +4,22 @@ const todoSchema = new mongoose.Schema(
   {
     content: {
       type: String,
+      required: true,
     },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    subTodos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubTodo",
+      },
+    ],
   },
   { timestamps: true },
 );

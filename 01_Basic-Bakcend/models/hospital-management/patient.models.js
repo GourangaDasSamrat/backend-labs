1147d6f4417d66b907d8b66a6035db9f3dcc8d67
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-13 12:07:16 +0600

    feat(patient): add patient model schema
    - Added name, age, gender fields
    - Included diagnosedWith, bloodGroup fields
    - Added address and admittedIn references

--- 01_Basic-Bakcend/models/hospital-management/patient.models.js
+++ 01_Basic-Bakcend/models/hospital-management/patient.models.js
@@ -1,5 +1,41 @@
 import mongoose from "mongoose";
 
-const patientSchema = new mongoose.Schema({}, { timestamps: true });
+const patientSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female"],
+      required: true,
+    },
+    diagnosedWith: {
+      type: String,
+      required: true,
+    },
+    bloodGroup: {
+      type: String,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+      required: true,
+    },
+    address: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Address",
+      },
+    ],
+    admittedIn: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hospital",
+    },
+  },
+  { timestamps: true },
+);
 
 export const Patient = mongoose.model("Patient", patientSchema);

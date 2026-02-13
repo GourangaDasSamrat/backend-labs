024860b1cd332d94163960519b725e1acd488cce
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-13 12:23:49 +0600

    feat(hospital-management): add Doctor model schema
    - Added name, address, salary, qualification fields
    - Included experienceInYears with default value
    - Implemented worksInHospitals subdocument

--- 01_Basic-Bakcend/models/hospital-management/doctor.models.js
+++ 01_Basic-Bakcend/models/hospital-management/doctor.models.js
@@ -1,5 +1,45 @@
 import mongoose from "mongoose";
 
-const doctorSchema = new mongoose.Schema({}, { timestamps: true });
+const hospitalHours = new mongoose.Schema({
+  hospital: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Hospital",
+  },
+  workHour: {
+    type: Number,
+    required: true,
+  },
+});
+
+const doctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    address: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Address",
+      },
+    ],
+    salary: {
+      type: String,
+      required: true,
+    },
+    qualification: {
+      type: String,
+      required: true,
+    },
+    experienceInYears: {
+      type: Number,
+      default: 0,
+    },
+    worksInHospitals: {
+      type: [hospitalHours],
+    },
+  },
+  { timestamps: true },
+);
 
 export const Doctor = mongoose.model("Doctor", doctorSchema);

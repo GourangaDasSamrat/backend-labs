ee10cb4b240401a706b83fe83196cdbb07292091
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-13 11:57:54 +0600

    feat(models): add address model
    - Created a new Mongoose schema for addresses
    - Defined fields: country, state, city, postcode
    - Added enum for country values

---
+++ 01_Basic-Bakcend/models/common/address.models.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    enum: [
+      "Bangladesh",
+      "United Kingdom",
+      "United States of America",
+      ,
+      "Canada",
+      "France",
+    ],
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  postcode: {
+    type: Number,
+  },
+});
+
+export const Address = mongoose.model("Address", addressSchema);

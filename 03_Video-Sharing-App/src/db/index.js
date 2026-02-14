f4f230af33499eb9eb6811361728ebb241d4f028
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-14 13:39:59 +0600

    feat(db): implement MongoDB Atlas connection
    - Added MONGODB_URI to .env.example
    - Created src/db/index.js for DB connection
    - Integrated connectDB in src/index.js

---
+++ 03_Video-Sharing-App/src/db/index.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(connectionInstance.connection.host);
+  } catch (err) {
+    console.error("Database connection error, ", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;

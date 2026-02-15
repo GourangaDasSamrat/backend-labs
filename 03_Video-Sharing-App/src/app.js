11571dddda82463145d9c34d728ffa94e34a6596
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-15 13:29:26 +0600

    feat(app): configure express middleware and server
    - Added CORS, JSON, URL encoder middleware
    - Configured static file serving and cookie parser
    - Implemented database connection and server start

---
+++ 03_Video-Sharing-App/src/app.js
@@ -0,0 +1,42 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+
+// initialize express app
+const app = express();
+
+// constant for limit
+const limit = "16kb";
+
+// configure cors for cross origin connection
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN, // frontend app's url
+    credentials: true, // allow credentials
+  })
+);
+
+// configure rate limiter for json
+app.use(
+  express.json({
+    limit,
+  })
+);
+
+// config url encoder
+app.use(
+  express.urlencoded({
+    extended: true,
+    // limit
+  })
+);
+
+// config for serve static files (in this case files inside public folder)
+app.use(express.static("public"));
+
+// config for secure cookies
+app.use(cookieParser());
+
+// export initialized express app
+export default app;
--- 03_Video-Sharing-App/src/index.js
+++ 03_Video-Sharing-App/src/index.js
@@ -1,5 +1,24 @@
 import "dotenv/config";
+import app from './app.js';
 import connectDB from "./db/index.js";
 
-// connect mongodb atlas
-connectDB();
+// connect database
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+    process.exit(1);
+  });
+
+  // use it when run locally
+if (process.env.NODE_ENV !== "production") {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server running locally on port ${port}`);
+  });
+}
+
+//  use export default for vercel
+export default app;

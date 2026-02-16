de0b9ef72682f960b5d9ad12e0ae3c9903a48430
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-16 12:25:41 +0600

    feat(user): add auth methods to user model
    - Add password hashing and comparison
    - Implement access and refresh token generation
    - Update dev dependencies for jsonwebtoken

--- 03_Video-Sharing-App/src/models/user.model.js
+++ 03_Video-Sharing-App/src/models/user.model.js
@@ -1,3 +1,6 @@
+import bcrypt from "bcrypt";
+import "dotenv/config";
+import jwt from "jsonwebtoken";
 import { Schema, Types, model } from "mongoose";
 
 const userSchema = new Schema(
@@ -62,5 +65,47 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// encrypt password before save in database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// decrypt password before match with database
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// generate access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = model("User", userSchema);

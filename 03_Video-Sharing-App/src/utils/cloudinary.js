428c2669104dcc12417d2953c4c331d1acb59ae7
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-17 11:44:15 +0600

    feat(cloudinary): add file upload utility
    - Implemented Cloudinary integration
    - Added uploadOnCloudinary function
    - Handles file upload and temp file deletion

---
+++ 03_Video-Sharing-App/src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import "dotenv/config";
+import fs from "fs";
+
+// cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// file upload
+export const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    //upload the file on cloudinary
+    const res = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File is uploaded on cloudinary", res.url);
+    return res;
+  } catch (err) {
+    // delete local temp file
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};

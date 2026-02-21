ec9b6ef427ea204f28eef30e395cd74fcb2e1f84
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-21 12:19:23 +0600

    feat(auth): implement user login functionality
    - Add generateAccessAndRefreshTokens helper
    - Implement login user controller logic
    - Handle user credential validation
    - Set access and refresh tokens in cookies

--- 03_Video-Sharing-App/src/controllers/user.controller.js
+++ 03_Video-Sharing-App/src/controllers/user.controller.js
@@ -5,6 +5,32 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary as upload } from "../utils/cloudinary.js";
 import { deleteLocalFile } from "../utils/fileHelper.js";
 
+// generate access & refresh tokens
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+
+    //generate tokens
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    // add refresh token in the user document
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  } catch (err) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating access & refresh tokens"
+    );
+  }
+};
+
+// register user
 export const registerUser = asyncHandler(async (req, res) => {
   // Extract & normalize fields
   let { userName, email, password, fullname } = req.body;
@@ -70,5 +96,67 @@ export const registerUser = asyncHandler(async (req, res) => {
   // Return response
   return res
     .status(201)
-    .json(new response(200, createdUser, "User registered successfully"));
+    .json(new response(201, createdUser, "User registered successfully"));
+});
+
+// login user
+export const loginUser = asyncHandler(async (req, res) => {
+  // Extract & normalize fields
+  let { userName, email, password } = req.body;
+  userName = userName?.trim().toLowerCase();
+  email = email?.trim().toLowerCase();
+
+  // Check username & email not exist
+  if (!userName || !email) {
+    throw new ApiError(400, "Username or email required");
+  }
+
+  // Check username or email
+  const user = await User.findOne({
+    $or: [{ userName }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  // Check password
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  //generate refresh and access tokens
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  // update refresh tokens on user
+  const loggedInUser = User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  // secure cookies options
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  // Return response
+  return res
+    .status(201)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new response(
+        201,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in successfully"
+      )
+    );
 });

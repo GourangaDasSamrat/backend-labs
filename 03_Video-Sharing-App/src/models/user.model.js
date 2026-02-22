14fc2049b3b69d66ee5a849227af638c75dce064
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-22 12:33:08 +0600

    fix(user): remove next from password hashing pre-save according to latest mongoose
    - Removed unnecessary `next()` call
    - Improved readability of pre-save hook

--- 03_Video-Sharing-App/src/models/user.model.js
+++ 03_Video-Sharing-App/src/models/user.model.js
@@ -66,8 +66,8 @@ const userSchema = new Schema(
 );
 
 // encrypt password before save in database
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   this.password = await bcrypt.hash(this.password, 10);
 });

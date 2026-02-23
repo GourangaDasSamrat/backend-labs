cd614355f8dc0babf35ea623c836a1830a2c6e73
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-23 18:12:29 +0600

    feat(user): add user profile and account management routes
    - Implemented routes for changing password
    - Added route to get current user details
    - Included routes for updating account details, avatar, and cover image

--- 03_Video-Sharing-App/src/routes/user.routes.js
+++ 03_Video-Sharing-App/src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import {
+  changePassword,
+  getCurrentUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
-  registerUser
+  registerUser,
+  updateAccountAvatar,
+  updateAccountCover,
+  updateAccountDetails,
 } from "../controllers/user.controller.js";
 import { verifyAuth } from "../middlewares/auth.middleware.js";
 import { multerUpload } from "../middlewares/multer.middleware.js";
@@ -32,6 +37,25 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyAuth, logoutUser);
 
 // refresh tokens
-router.route("/refresh_tokens").post(refreshAccessToken);
+router.route("/refresh-tokens").post(refreshAccessToken);
+
+// change password
+router.route("/change-password").post(verifyAuth, changePassword);
+
+// get current user
+router.route("/current-user").get(verifyAuth, getCurrentUser);
+
+// update account details
+router.route("/update-account").patch(verifyAuth, updateAccountDetails);
+
+// update account avatar
+router
+  .route("/avatar")
+  .patch(verifyAuth, multerUpload.single("avatar"), updateAccountAvatar);
+
+// update account cover
+router
+  .route("/cover-image")
+  .patch(verifyAuth, multerUpload.single("coverImage"), updateAccountCover);
 
 export default router;

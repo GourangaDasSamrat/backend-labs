ddca05f12d51804385c3b056ece965fffefdb1d1
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-26 12:19:04 +0600

    fix: fix Post model import path
    - Corrected import path for Post model
    - Ensured consistent naming convention

--- 03_Video-Sharing-App/src/controllers/post.controller.js
+++ 03_Video-Sharing-App/src/controllers/post.controller.js
@@ -1,5 +1,5 @@
 import mongoose, { isValidObjectId } from "mongoose";
-import { Post } from "../models/Post.model.js";
+import { Post } from "../models/post.model.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";

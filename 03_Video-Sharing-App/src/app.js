3a347f3ab958901310be2b4400b62c6d3f37e5ad
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-26 11:15:03 +0600

    style: Add semicolons to route declarations
    - Added missing semicolons to route definitions
    - Ensured consistent syntax in app.js
    - Updated export statements in index.js

--- 03_Video-Sharing-App/src/app.js
+++ 03_Video-Sharing-App/src/app.js
@@ -84,19 +84,19 @@ import {
   postRouter,
   subscriptionRouter,
   userRouter,
-  videoRouter
+  videoRouter,
 } from "./routes/index.js";
 
 // routes declaration
-app.use("/api/v1/healthcheck", healthcheckRouter)
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/posts", postRouter)
-app.use("/api/v1/subscriptions", subscriptionRouter)
-app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/comments", commentRouter)
-app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 // export initialized express app
 export default app;

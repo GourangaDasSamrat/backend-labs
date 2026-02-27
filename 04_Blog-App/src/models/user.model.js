03188c48730a4addf7e17d31a4a49c31bb50fc90
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-27 15:04:10 +0600

    feat(user): implement user model with hashing
    - Define user schema with Mongoose
    - Add password hashing on save
    - Implement password matching for login

---
+++ 04_Blog-App/src/models/user.model.js
@@ -0,0 +1,93 @@
+import { Schema, model } from "mongoose";
+import { createHmac, randomBytes } from "node:crypto";
+import { ApiError } from "../utils/ApiError.js";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      minlength: [3, "Username must be at least 3 characters"],
+      maxlength: [20, "Username can't exceed 20 characters"],
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please use a valid email address",
+      ],
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
+    },
+
+    salt: {
+      type: String,
+    },
+
+    fullname: {
+      type: String,
+      required: [true, "Full name is required"],
+      trim: true,
+      index: true,
+    },
+
+    avatar: {
+      type: String,
+      default: "/img/avatar.png",
+    },
+
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true },
+);
+
+// hash password before save in database
+userSchema.pre("save", function () {
+  if (!this.isModified("password")) return;
+
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(this.password)
+    .digest("hex");
+
+  this.salt = salt;
+  this.password = hashedPassword;
+});
+
+// hash password and check when signin
+userSchema.static("matchPassword", function (email, password) {
+  const user = this.findOne({ email });
+
+  if (!user) throw new ApiError(404, "User not found");
+
+  const salt = user.salt;
+  const hashedPassword = user.password;
+
+  const userProvidedHash = createHmac("sha256", salt)
+    .update(password)
+    .digest("hex");
+
+  if (hashedPassword !== userProvidedHash)
+    throw new ApiError(400, "Incorrect password");
+
+  return { ...user, password: undefined, salt: undefined };
+});
+
+export const User = model("User", userSchema);

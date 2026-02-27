d715d2dbd5a1ece538636512f0bd328c87ff0a89
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-27 12:11:46 +0600

    feat(app): configure express with cors and json limit
    - Added cors middleware for cross-origin requests
    - Configured express.json with a 16kb limit
    - Added a route to render the home view

--- 04_Blog-App/src/app.js
+++ 04_Blog-App/src/app.js
@@ -1,14 +1,37 @@
-import path from'path'
-import express from'express'
+import cors from "cors";
+import express from "express";
+import path from "path";
 
 // initialize express app
 const app = express();
+
 // configure port
 export const port = process.env.PORT || 3000;
 
+// constant for limit
+const limit = "16kb";
+
+// configure cors for cross origin connection
+app.use(
+  cors({
+    origin: process.env.CORS_ALLOWED_ORIGINS, // frontend app's url
+    credentials: true, // allow credentials
+  }),
+);
+
+// configure rate limiter for json
+app.use(
+  express.json({
+    limit,
+  }),
+);
+
 // configure ejs
-app.set('view engine','ejs')
-app.set('views',path.resolve('./views'))
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./src/views"));
+
+// serve page
+app.get("/", (req, res) => res.render("home"));
 
 // export initialized express app
 export default app;

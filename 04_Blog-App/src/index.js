b4b9ae0f141ff313529b170c1f1c885df92ba73b
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-27 11:10:56 +0600

    feat(app): initialize app and connect DB
    - Import dotenv for environment variables
    - Import app and port from app.js
    - Connect to the database on startup
    - Start server locally if not in production
    - Export app as default for Vercel deployment

---
+++ 04_Blog-App/src/index.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import app, { port } from "./app.js";
+import connectDB from "./db/index.js";
+
+// connect database
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+    process.exit(1);
+  });
+
+// use it when run locally
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Server running locally on port ${port}`);
+  });
+}
+
+//  use export default for vercel
+export default app;

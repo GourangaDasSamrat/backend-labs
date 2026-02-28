329972d95a706ed7cd7a04658dcaf63aba52d806
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-28 12:53:53 +0600

    feat(auth): add token creation and validation
    - Implemented JWT token creation for users
    - Added JWT token validation logic
    - Utilizes environment variable for secret

---
+++ 04_Blog-App/src/utils/auth.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+const secret = process.env.JWT_TOKEN_SECRET;
+
+export const createTokenForUser = (user) => {
+  const payload = {
+    _id: user._id,
+    email: user.email,
+    username: user.username,
+    avatar: user.avatar,
+    role: user.role,
+  };
+  const token = jwt.sign(payload, secret);
+
+  return token;
+};
+
+export const validateToken = (token) => {
+  const payload = jwt.verify(token, secret);
+
+  return payload;
+};

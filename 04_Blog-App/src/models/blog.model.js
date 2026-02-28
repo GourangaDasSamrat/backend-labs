bf41156470b441a58a996e8fd2841b24fd25cd0c
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-28 14:35:48 +0600

    feat(blog): add blog model
    - Defines Mongoose schema for blog posts
    - Includes title, body, coverImage, and author
    - Enforces character limits for title and body
    - Adds timestamp for creation and updates

---
+++ 04_Blog-App/src/models/blog.model.js
@@ -0,0 +1,28 @@
+import { Schema, Types, model } from "mongoose";
+
+const blogSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minlength: [15, "Blog title must be at least 15 characters"],
+      maxlength: [72, "Blog title can't exceed 72 characters"],
+    },
+    body: {
+      type: String,
+      required: true,
+      minlength: [30, "Blog content must be at least 30 characters"],
+      maxlength: [500, "Blog content can't exceed 500 characters"],
+    },
+    coverImage: {
+      type: String,
+    },
+    createdBy: {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true },
+);
+
+export const Blog = model("Blog", blogSchema);

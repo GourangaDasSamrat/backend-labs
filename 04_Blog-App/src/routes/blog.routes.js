c23e00c224e6f0753614a6b468a2a9548c9d968f
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-28 16:44:20 +0600

    feat(blog): add new blog post route
    - Implemented POST route for /add-new
    - Integrated multer for image upload
    - Connected to blog controllers

--- 04_Blog-App/src/routes/blog.routes.js
+++ 04_Blog-App/src/routes/blog.routes.js
@@ -1,8 +1,11 @@
+import{multerUpload}from'../middlewares/multer.middleware.js'
 import { Router } from "express";
+import { addNewBlog } from "../controllers/blog.controllers.js";
 
 const route = Router();
 
 // add new blog route
 route.get("/add-new", (req, res) => res.render("addBlog", { user: req.user }));
+route.post("/add-new", multerUpload.single("coverImage"), addNewBlog);
 
 export default route;

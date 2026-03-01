30ba59eff17df4ff20a5230856339436e829b546
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-01 12:03:14 +0600

    feat(comment): add new comment controller
    - Implements controller for adding new comments
    - Validates comment body and blog ID
    - Associates comment with logged-in user

---
+++ 04_Blog-App/src/controllers/comment.controllers.js
@@ -0,0 +1,21 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+// import {Comment}from'../models/comment.model.js'
+
+export const addNewComment = asyncHandler(async (req, res) => {
+  // extract comment body and blog id
+  const { body } = req.body;
+  const blogId = req.params.blogId;
+
+  if (!body || body.trim() === "") {
+    return res.status(400).json({ message: "Comment body is required" });
+  }
+
+  // add comment on db
+  await Comment.create({
+    content: body,
+    blogId,
+    createdBy: req.user._id,
+  });
+
+  return res.redirect(`/blog/${blogId}`);
+});

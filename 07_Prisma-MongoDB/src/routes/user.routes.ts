212eaa827ade85a1f30d8e11e8e23be720b1fed8
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-04 12:52:21 +0600

    fix(routes): correct trailing semicolon and spacing
    - Removed trailing semicolon from import
    - Adjusted spacing in router initialization
    - Corrected spacing in route definition

--- 07_Prisma-MongoDB/src/routes/user.routes.ts
+++ 07_Prisma-MongoDB/src/routes/user.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import {handleUserSignUp} from '../controllers/user.controllers'
+import { handleUserSignUp } from "../controllers/user.controllers";
 
-const router= Router()
+const router = Router();
 
 // sign up
-router.route('/signup').post(handleUserSignUp)
+router.route("/signup").post(handleUserSignUp);
 
-export default router
+export default router;

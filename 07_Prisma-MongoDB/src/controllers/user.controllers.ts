8691c39101c940bc42560ee3c14bf06ddb4a26ce
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-05 10:41:10 +0600

    feat(user): add user sign in functionality
    - Implemented user sign in logic
    - Validates email and password
    - Finds user and checks credentials
    - Sends cookie on successful sign in

--- 07_Prisma-MongoDB/src/controllers/user.controllers.ts
+++ 07_Prisma-MongoDB/src/controllers/user.controllers.ts
@@ -3,7 +3,7 @@ import prisma from "@db";
 import { Request, Response } from "express";
 import { ApiError } from "../utils/apiError";
 import { asyncHandler } from "../utils/asyncHandler";
-import { cookieToken } from '../utils/cookieToken';
+import { cookieToken } from "../utils/cookieToken";
 
 // user sign up
 export const handleUserSignUp = asyncHandler(
@@ -26,8 +26,46 @@ export const handleUserSignUp = asyncHandler(
       });
 
       // send user cookie
-      cookieToken(user,res)
+      cookieToken(user, res);
+    } catch (err) {
+      if (err instanceof Error) {
+        throw new ApiError(500, err.message, [err]);
+      }
+      throw new ApiError(500, "Something went wrong", [err]);
+    }
+  },
+);
 
+// user sign in
+export const handleUserSignIn = asyncHandler(
+  async (req: Request, res: Response) => {
+    try {
+      // extract data from request body
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        throw new ApiError(400, "All fields required");
+      }
+
+      // find user on db
+      const user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      // if user not exist
+      if (!user) {
+        throw new ApiError(404, "User not found");
+      }
+
+      // if password not matched
+      if (user.password !== password) {
+        throw new ApiError(400, "Invalid password");
+      }
+
+      // send user cookie
+      cookieToken(user, res);
     } catch (err) {
       if (err instanceof Error) {
         throw new ApiError(500, err.message, [err]);

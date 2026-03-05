854c1d348df9bbf8db7b3cd797dcd8ab3a051f76
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-05 10:41:26 +0600

    feat(user): add sign-in route
    - Added GET endpoint for user sign-in
    - Imported handleUserSignIn controller

--- 07_Prisma-MongoDB/src/routes/user.routes.ts
+++ 07_Prisma-MongoDB/src/routes/user.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { handleUserSignUp } from "../controllers/user.controllers";
+import { handleUserSignIn, handleUserSignUp } from "../controllers/user.controllers";
 
 const router = Router();
 
 // sign up
 router.route("/signup").post(handleUserSignUp);
+
+// sign in
+router.route("/signin").get(handleUserSignIn);
 
 export default router;

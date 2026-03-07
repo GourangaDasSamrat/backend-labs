5aa02d28367848b7e2272f8dc3235d93815b6df5
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-07 17:59:40 +0600

    refactor(post): remove redundant try-catch blocks
    - Simplified error handling
    - Removed unnecessary try-catch statements
    - Improved code readability

--- express/07_Prisma-MongoDB/src/controllers/post.controllers.ts
+++ express/07_Prisma-MongoDB/src/controllers/post.controllers.ts
@@ -5,92 +5,67 @@ import { Request, Response } from "express";
 
 // create new post
 export const handleCreatePost = async (req: Request, res: Response) => {
-  try {
-    // extract data from request body
-    let { slug, title, content, authorId } = req.body;
-    slug = slug?.trim();
-    title = title?.trim();
-    content = content?.trim();
+  let { slug, title, content, authorId } = req.body;
 
-    if (!slug || !title || !content || !authorId) {
-      throw new ApiError(400, "All fields required");
-    }
+  slug = slug?.trim();
+  title = title?.trim();
+  content = content?.trim();
 
-    // create post
-    const createPost: Post = await prisma.post.create({
-      data: {
-        slug,
-        title,
-        content,
-        author: { connect: { id: authorId } },
-      },
-    });
-
-    res.status(200).json({ success: true, createPost });
-  } catch (err) {
-    if (err instanceof Error) {
-      throw new ApiError(500, err.message, [err]);
-    }
-    throw new ApiError(500, "Something went wrong", [err]);
+  if (!slug || !title || !content || !authorId) {
+    throw new ApiError(400, "All fields required");
   }
+
+  const createPost: Post = await prisma.post.create({
+    data: {
+      slug,
+      title,
+      content,
+      author: { connect: { id: authorId } },
+    },
+  });
+
+  res.status(200).json({ success: true, createPost });
 };
 
 // update post
 export const handleUpdatePost = async (req: Request, res: Response) => {
-  try {
-    // extract post id
-    const { id } = req.params;
+  const { id } = req.params;
 
-    // validate id
-    if (!id || Array.isArray(id)) {
-      throw new ApiError(400, "Post does not exist");
-    }
-
-    // extract data from request body
-    let { title, content } = req.body;
-    title = title?.trim();
-    content = content?.trim();
-
-    if (!title || !content) {
-      throw new ApiError(400, "All fields required");
-    }
-
-    // update post
-    const updatedPost = await prisma.post.update({
-      where: { id },
-      data: {
-        title,
-        content,
-      },
-    });
-
-    res.status(200).json({ success: true, updatedPost });
-  } catch (err) {
-    if (err instanceof Error) {
-      throw new ApiError(500, err.message, [err]);
-    }
-    throw new ApiError(500, "Something went wrong", [err]);
+  if (!id || Array.isArray(id)) {
+    throw new ApiError(400, "Post does not exist");
   }
+
+  let { title, content } = req.body;
+
+  title = title?.trim();
+  content = content?.trim();
+
+  if (!title || !content) {
+    throw new ApiError(400, "All fields required");
+  }
+
+  const updatedPost = await prisma.post.update({
+    where: { id },
+    data: {
+      title,
+      content,
+    },
+  });
+
+  res.status(200).json({ success: true, updatedPost });
 };
 
 // delete post
 export const handleDeletePost = async (req: Request, res: Response) => {
-  try {
-    // extract post id
-    const { id } = req.params;
+  const { id } = req.params;
 
-    // validate id
-    if (!id || Array.isArray(id)) {
-      throw new ApiError(400, "Post does not exist");
-    }
-
-    const deletePost = prisma.post.delete({ where: { id } });
-
-    res.status(200).json({ success: true, deletePost});
-  } catch (err) {
-    if (err instanceof Error) {
-      throw new ApiError(500, err.message, [err]);
-    }
-    throw new ApiError(500, "Something went wrong", [err]);
+  if (!id || Array.isArray(id)) {
+    throw new ApiError(400, "Post does not exist");
   }
+
+  const deletePost = await prisma.post.delete({
+    where: { id },
+  });
+
+  res.status(200).json({ success: true, deletePost });
 };

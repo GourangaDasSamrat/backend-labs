88545593ea307baf61d8008602cd0ad93ef2f725
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-26 17:51:20 +0600

    feat(backend): add basic server setup
    - Implemented http server
    - Integrated gorilla/mux router
    - Added logging for server start

--- go/01_basic-backend/main.go
+++ go/01_basic-backend/main.go
@@ -1,10 +1,19 @@
 package main
 
-import "github.com/gourangadassamrat/backend-labs/go/01_basic-backend/models"
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-// dummy db
-var Courses []models.Course
+	"github.com/gourangadassamrat/backend-labs/go/01_basic-backend/routes"
+)
 
 func main() {
+    // Get the router from the routes package
+    r := routes.Router()
 
+    fmt.Println("Server is starting on port 8080...")
+
+    // ListenAndServe returns an error, so we check it
+    log.Fatal(http.ListenAndServe(":8080", r))
 }

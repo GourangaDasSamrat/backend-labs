c9bd1f2431304b30282aa5d983c1ad71d4054013
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-30 11:00:58 +0600

    fix: Improve error handling in response utilities
    - Log JSON encoding errors instead of using http.Error
    - Log HTML write errors to satisfy linter

--- go/01_basic-backend/utils/response.go
+++ go/01_basic-backend/utils/response.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -13,8 +14,9 @@ func JSONResponse(w http.ResponseWriter, code int, data interface{}) {
 	// If there is no data to send (like a 404 or 204), don't try to encode.
 	if data != nil {
 		if err := json.NewEncoder(w).Encode(data); err != nil {
-			// If encoding fails, we've already sent a header, so we just log.
-			http.Error(w, "Error encoding response", http.StatusInternalServerError)
+			// If encoding fails, we log it.
+			// We can't use http.Error here because WriteHeader was already called.
+			log.Printf("Error encoding JSON response: %v", err)
 		}
 	}
 }
@@ -27,5 +29,9 @@ func JSONError(w http.ResponseWriter, code int, message string) {
 // WriteHTML is a clean way to send non-JSON responses.
 func WriteHTML(w http.ResponseWriter, html string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Write([]byte(html))
+	// FIX: Check the error return value of w.Write to satisfy the linter.
+	// We use the blank identifier (_) for the byte count and check the error.
+	if _, err := w.Write([]byte(html)); err != nil {
+		log.Printf("Error writing HTML response: %v", err)
+	}
 }

007e70bc205aaa0c38830fdf09f293eb17da64f9
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-04-16 12:27:45 +0600

    feat(api): implement basic HTTP server and graceful shutdown
    - Load configuration from environment
    - Set up a simple HTTP router
    - Handle graceful shutdown with context timeout

--- go/basic/03_students-api/cmd/students-api/main.go
+++ go/basic/03_students-api/cmd/students-api/main.go
@@ -1,9 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gourangadassamrat/students-api/internal/config"
 )
 
 func main() {
-	fmt.Println()
+	cfg := config.MustLoad()
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		if _, err := w.Write([]byte("Welcome to students api")); err != nil {
+			slog.Error("Failed to write response", "error", err)
+		}
+	})
+
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	slog.Info("Server started", "port", cfg.Address)
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	<-done
+
+	slog.Info("Shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown", "error", err)
+	}
+
+	slog.Info("Server shutting down successfully")
+
 }

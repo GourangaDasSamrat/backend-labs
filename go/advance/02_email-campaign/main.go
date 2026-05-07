eff61916353582c3b481bd46ed4eff1634209bd1
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-05-07 12:56:16 +0600

    feat(email): implement concurrent email sending
    - Introduce Recipient struct
    - Use channels for recipient communication
    - Implement worker pool for sending emails
    - Utilize sync.WaitGroup for graceful shutdown

--- go/advance/02_email-campaign/main.go
+++ go/advance/02_email-campaign/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"sync"
 )
 
+type Recipient struct {
+	Name  string
+	Email string
+}
+
 func main() {
-	fmt.Println("Email dispatcher")
+	recipientChannel := make(chan Recipient)
+
+	go loadRecipient("./emails.csv", recipientChannel)
+
+	var wg sync.WaitGroup
+	workerCount := 5
+
+	for i := 1; i <= workerCount; i++ {
+		wg.Add(1)
+		go emailWorker(i, recipientChannel, &wg)
+	}
+
+	wg.Wait()
 }
